a3ec13ee238e89d224ff6b4e3592c16b52c4f980
Mayamner <mem.kpope@gmail.com>
2026-06-12 16:23:44 +0300

    s

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-new_str = str[26:] 
-print(new_str)
+print(str[39:112] + str[0:6])
