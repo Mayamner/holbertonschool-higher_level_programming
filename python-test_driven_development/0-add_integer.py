f024127ec2d95a25ceb379a4cd83c272cf7756d4
Mayamner <mem.kpope@gmail.com>
2026-06-13 22:20:29 +0300

    Add task 0 - add integer

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    """Adds 2 integers.
+
+    Args:
+        a: first integer
+        b: second integer (default 98)
+
+    Returns:
+        The addition of a and b
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
