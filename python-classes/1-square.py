0d38b27c24d6ce661e200ba0d7f64fa5df40562b
Mayamner <mem.kpope@gmail.com>
2026-06-19 23:30:58 +0300

    s

---
+++ python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This module defines a Square class."""
+
+
+class Square:
+    """This class defines a square."""
+
+    def __init__(self, size):
+        """Initialize a new Square.
+
+        Args:
+            size: The size of the square.
+        """
+        self.__size = size
+        
