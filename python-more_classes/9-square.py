8db84d02a9f73d9de70e6d101e7c114572ac13a8
Mayamner <mem.kpope@gmail.com>
2026-06-20 23:36:42 +0300

    s

--- python-more_classes/9-square.py
+++ python-more_classes/9-square.py
@@ -1,30 +1,104 @@
 #!/usr/bin/python3
-"""This module defines a Square class, based on Rectangle."""
-Rectangle = __import__('9-rectangle').Rectangle
+"""This module defines a Rectangle class."""
 
 
-class Square(Rectangle):
-    """This class defines a square, inherited from Rectangle."""
+class Rectangle:
+    """This class defines a rectangle."""
 
-    def __init__(self, size=0):
-        """Initialize a new Square.
+    number_of_instances = 0
+    print_symbol = "#"
+
+    def __init__(self, width=0, height=0):
+        """Initialize a new Rectangle.
 
         Args:
-            size (int): The size of the new square.
+            width (int): The width of the new rectangle.
+            height (int): The height of the new rectangle.
         """
-        super().__init__(size, size)
-
-    def __str__(self):
-        """Return the printable representation of the square."""
-        return "[Square] {}/{}".format(self.width, self.height)
+        self.width = width
+        self.height = height
+        Rectangle.number_of_instances += 1
 
     @property
-    def size(self):
-        """Get/set the size of the square."""
-        return self.width
+    def width(self):
+        """Get/set the width of the rectangle."""
+        return self.__width
 
-    @size.setter
-    def size(self, value):
-        """Set size by reusing the width setter's validation."""
-        self.width = value
-        self.height = value
+    @width.setter
+    def width(self, value):
+        """Set width with validation."""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Get/set the height of the rectangle."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set height with validation."""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Return the area of the rectangle."""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Return the perimeter of the rectangle."""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        """Return the printable representation of the rectangle."""
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        rows = [str(self.print_symbol) * self.__width
+                for _ in range(self.__height)]
+        return "\n".join(rows)
+
+    def __repr__(self):
+        """Return a string representation that can recreate the object."""
+        return "Rectangle({}, {})".format(self.__width, self.__height)
+
+    def __del__(self):
+        """Print a message when an instance is deleted."""
+        print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        """Return the bigger rectangle, based on the area.
+
+        Args:
+            rect_1 (Rectangle): The first rectangle to compare.
+            rect_2 (Rectangle): The second rectangle to compare.
+        Returns:
+            rect_1 if its area >= rect_2's area, otherwise rect_2.
+        """
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area() >= rect_2.area():
+            return rect_1
+        return rect_2
+
+    @staticmethod
+    def square(size=0):
+        """Return a new Rectangle instance with width == height == size.
+
+        Args:
+            size (int): The width and height of the new rectangle.
+        Returns:
+            A new Rectangle instance.
+        """
+        return Rectangle(size, size)
